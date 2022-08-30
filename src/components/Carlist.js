d37b571bbe3fd813665f701ff3ce293fc8f34bcb
Samuel Herrmann <samuelherrmann@gmx.net>
2022-08-30 10:05:18 +0100

    use DataGrid

--- src/components/Carlist.js
+++ src/components/Carlist.js
@@ -1,7 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { SERVER_URL } from '../constants.js'
+import { DataGrid } from '@mui/x-data-grid';
+
 function Carlist() {
     const [cars, setCars] = useState([]);
+    const columns = [
+        {field: 'brand', headerName: 'Brand', width: 200},
+        {field: 'model', headerName: 'Model', width: 200},
+        {field: 'color', headerName: 'Color', width: 200},
+        {field: 'year', headerName: 'Year', width: 150},
+        {field: 'price', headerName: 'Price', width: 150},
+    ]
     useEffect( () => {
         fetch(SERVER_URL + 'api/cars')
         .then(response => response.json())
@@ -9,22 +18,11 @@ function Carlist() {
         .catch(err => console.error(err));
     }, []);
     return (
-        <div>
-            <table>
-                <tbody>
-                    {
-                        cars.map( (car, index) =>
-                        <tr key={index}>
-                            <td>{car.brand}</td>
-                            <td>{car.model}</td>
-                            <td>{car.color}</td>
-                            <td>{car.year}</td>
-                            <td>{car.price}</td>
-                        </tr>
-                        )
-                    }
-                </tbody>
-            </table>
+        <div style={{ height: 500, width: '80%' }}>
+            <DataGrid
+                rows={cars}
+                columns={columns}
+                getRowId={ row => row._links.self.href}/>
         </div>
     );
 }
